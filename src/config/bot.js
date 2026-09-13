d16c56e6ba9e5dcc9119a48d26e94cd8e33df8a4
jesco44867-svg <jesco44867@gmail.com>
2026-09-13 06:14:58 -0700

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -93,8 +93,8 @@ export const botConfig = {
   embeds: {
     colors: {
       // Main brand colors.
-      primary: "#336699",
-      secondary: "#2F3136",
+      primary: "#9999ff",
+      secondary: "#FF000D",
 
       // Standard status colors for success/error/warning/info messages.
       success: "#57F287",
